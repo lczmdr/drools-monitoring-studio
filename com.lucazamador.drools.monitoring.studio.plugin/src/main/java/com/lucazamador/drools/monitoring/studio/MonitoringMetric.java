06fc8e79fe5553f7c19960535820394681d654a4
Lucas Amador <lucazamador@gmail.com>
2011-06-12 02:08:57 -0300

    more available metrics added

--- com.lucazamador.drools.monitoring.studio.plugin/src/main/java/com/lucazamador/drools/monitoring/studio/MonitoringMetric.java
+++ com.lucazamador.drools.monitoring.studio.plugin/src/main/java/com/lucazamador/drools/monitoring/studio/MonitoringMetric.java
@@ -2,8 +2,15 @@ package com.lucazamador.drools.monitoring.studio;
 
 public enum MonitoringMetric {
 
-    AVERATE_FIRING_TIME("Average Firing Time"), TOTAL_ACTIVATIONS_CREATED("Total Activations Created"), TOTAL_ACTIVATIONS_CANCELED(
-            "Total Activations Canceled"), TOTAL_ACTIVATIONS_FIRED("Total Activations Fired");
+    AVERATE_FIRING_TIME("Average Firing Time"),
+    LAST_REST("Last Reset"),
+    TOTAL_ACTIVATIONS_CANCELED("Total Activations Canceled"), 
+    TOTAL_ACTIVATIONS_CREATED("Total Activations Created"),
+    TOTAL_ACTIVATIONS_FIRED("Total Activations Fired"),
+    TOTAL_FACT_COUNT("Total Fact Count"),
+    TOTAL_FIRING_TIME("Total Firing Time"),
+    TOTAL_PROCESS_INSTANCES_COMPLETED("Total Process Instances Completed"),
+    TOTAL_PROCESS_INSTANCES_STARTED("Total Process Instances Started");
 
     private final String name;
 
